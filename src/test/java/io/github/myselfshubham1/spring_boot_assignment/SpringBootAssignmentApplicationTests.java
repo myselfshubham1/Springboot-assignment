c596d726bd0abef0d5434e645cc464ddab3bc467
Shubahm kumar <s9977365@gmail.com>
2025-04-13 10:49:58 +0530

    Initial commit

---
+++ src/test/java/io/github/myselfshubham1/spring_boot_assignment/SpringBootAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.myselfshubham1.spring_boot_assignment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
