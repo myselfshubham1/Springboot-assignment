cc03278bb0d179283c00cf06b7a5631ff08c322e
SiddharthTrips95 <siddharthtrips9115+github@gmail.com>
2025-04-13 23:55:19 +0530

    user

---
+++ src/main/java/io/github/myselfshubham1/spring_boot_assignment/user/services/UserService.java
@@ -0,0 +1,13 @@
+package io.github.myselfshubham1.spring_boot_assignment.user.services;
+
+import io.github.siddharthtrips95.userspring.user.model.User;
+
+import java.util.List;
+
+public interface UserService {
+    User createUser(User user);
+    User getUserById(Long id);
+    List<User> getAllUsers();
+    User updateUser(Long id, User updatedUser);
+    boolean deleteUser(Long id);
+}
